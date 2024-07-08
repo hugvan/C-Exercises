5fd4d15eae2a7038a78bae4f1f55cfe6d4250ebf
Vaughn Iggie Aquino <vraquino@up.edu.ph>
2024-07-08 17:16:41 +0800

    Add Convert to Double

--- pointers.c
+++ pointers.c
@@ -5,14 +5,26 @@ void swap(int *i, int *j);
 bool search(const int arr[], int size, int key);
 void print_bool(bool x);
 double inner_product(const double *arr1, const double *arr2, int size);
+void convert_to_double(const int* arr1, int size, double* arr2 );
 
 int main(void) {
 
     int arr1[] = {2, 3, 5, 7, 11, 13};
+    int arr2[] = {4, 0, 2, -1, 0, 0};
+    /*
     print_bool( search(arr1, 6, 3));
     print_bool( search(arr1, 6, 4));
     print_bool( search(arr1, 6, 2));
     print_bool( search(arr1, 6, 13));
+     */
+
+    double d1[6] = {0};
+    double d2[6] = {0};
+    convert_to_double(arr1, 6, d1);
+    convert_to_double(arr2, 6, d2);
+
+    printf("%f \n", inner_product(d1, d2, 6) );
+
     return 0;
 }
 
@@ -39,4 +51,10 @@ double inner_product(const double *arr1, const double *arr2, int size) {
         product += *(arr1 + i) * *(arr2 + i);
     }
     return product;
+}
+
+void convert_to_double(const int* arr1, int size, double* arr2 ) {
+    for (int i = 0; i < size; i++) {
+        arr2[i] = (double) arr1[i];
+    }
 }
